1ac2a6dadf65a859f502c6253a4fdd72fb8bf495
Gustavo-Nascimentoo <gustavoalm1202@gmail.com>
2026-05-12 21:17:03 -0300

    fix/funcionalidades
    Arrumei o botão cancelar dos htmls de Login e Cadastro e alterei a mensagem do JavaScript de login

---
+++ JavaScript/login.js
@@ -0,0 +1,27 @@
+document.querySelector('.btn-cadastro-login').addEventListener('click', () => {
+    const email = document.querySelector('input[type="text"]').value.trim();
+    const senha = document.querySelector('input[type="password"]').value.trim();
+
+    if (!email) {
+        alert('Por favor, preencha o campo de e-mail.');
+        return;
+    }
+
+    if (!email.includes('@') || !email.includes('.com') || email.includes(' ')) {
+        alert('Digite um e-mail válido. Ex: nome@email.com');
+        return;
+    }
+
+    if (!senha) {
+        alert('Por favor, preencha o campo de senha.');
+        return;
+    }
+
+    if (senha.length < 8) {
+        alert('A senha deve ter pelo menos 8 caracteres.');
+        return;
+    }
+
+    alert(`Login efetuado com sucesso! Bem-vindo ao OptiCam.`);
+    console.log(`Email: ${email} | Senha: ${senha}`);
+});
